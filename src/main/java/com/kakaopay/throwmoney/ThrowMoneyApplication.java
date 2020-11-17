b00244e250b39b5e2f2a0025b333f136c6ac7cd7
신국정 <orkaye7751@gmail.com>
2020-11-17 22:14:06 +0900

    project init

---
+++ src/main/java/com/kakaopay/throwmoney/ThrowMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.throwmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrowMoneyApplication.class, args);
+	}
+
+}
